3047486892d7bb09ae430cd8b9f009bfe2215377
mohamedabdimohammud <mmohammud@outlook.com>
2023-04-09 17:01:49 +0300

    Completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # phase-1-project
+Man Utd Fan Website - README
+
+This is the README file for the Man Utd fan website, a website dedicated to providing news, updates, and information about Manchester United Football Club.
+Description
+
+The Man Utd fan website is designed to provide a platform for fans of Manchester United to stay up-to-date with the latest news, transfers, fixtures, and other information related to the club. The website is created by a group of passionate Manchester United supporters who are dedicated to sharing their love for the club with fellow fans.
+Features
+
+The website includes the following features:
+
+    A landing page that welcomes users to the website and provides a brief overview of the website's contents.
+    A navigation bar that allows users to easily navigate to different sections of the website, including News, Squad, Team History, and Store.
+    A "News" section that provides the latest news and updates related to Manchester United.
+    A "Transfers" section that showcases the latest transfer news and rumors surrounding the club.
+    A "Fixtures" section that provides information about upcoming matches and past results.
+
+Technologies Used
+
+The website was created using the following technologies:
+
+    HTML
+    CSS
+    JavaScript
+    Bootstrap
+    jQuery
+
+Installation
+
+No installation is necessary to view the website. Simply navigate to the website's URL to view the website.
+
+License
+
+The license used to help create this project is the Unlicense. Please find more information in the provided license document.
--- home.js
+++ home.js
@@ -1,26 +1,26 @@
 const newsBtn = document.querySelector('#newsBtn');
-      const squadBtn = document.querySelector('#squadBtn');
-      const historyBtn = document.querySelector('#historyBtn');
-      const storeBtn = document.querySelector('#storeBtn');
+const squadBtn = document.querySelector('#squadBtn');
+const historyBtn = document.querySelector('#historyBtn');
+const storeBtn = document.querySelector('#storeBtn');
 
-      newsBtn.addEventListener('click', function() {
-        window.location.href = data.newsUrl;
-      });
+newsBtn.addEventListener('click', function() {
+  window.location.href = data.newsUrl;
+});
 
-      squadBtn.addEventListener('click', function() {
-        window.location.href = data.squadUrl;
-      });
+squadBtn.addEventListener('click', function() {
+  window.location.href = data.squadUrl;
+});
 
-      historyBtn.addEventListener('click', function() {
-        window.location.href = data.historyUrl;
-      });
-      storeBtn.addEventListener('click', function() {
-    window.location.href = data.storeUrl;
-      });
+historyBtn.addEventListener('click', function() {
+  window.location.href = data.historyUrl;
+});
+storeBtn.addEventListener('click', function() {
+window.location.href = data.storeUrl;
+});
 
-  const data = {
-    newsUrl: "http://127.0.0.1:5500/news.html",
-    squadUrl: "http://127.0.0.1:5500/squad.html",
-    historyUrl: "http://127.0.0.1:5500/history.html",
-    storeUrl: "http://127.0.0.1:5500/store.html"
-  };
+const data = {
+newsUrl: "https://mohamedabdimohammud.github.io/phase-1-project-news/",
+squadUrl: "https://mohamedabdimohammud.github.io/phase-1-project-squad/",
+historyUrl: "https://mohamedabdimohammud.github.io/phase-1-project-history/",
+storeUrl: "https://mohamedabdimohammud.github.io/phase-1-project-store/"
+};
